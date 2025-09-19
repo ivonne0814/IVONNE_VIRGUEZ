655baf9f4a5a76f7230610ee92ea77300fcb1d68
ivonne <xiomararico.xv@gmail.com>
2025-09-19 18:27:40 -0500

    Primer commit de primera-app

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { ClientController } from './client/client.controller';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CashierModule } from './cashier/cashier.module';
+import { CoffeeModule } from './coffee/coffee.module';
+import { ClientModule } from './client/client.module';
+import { CoffeeController } from './coffee/coffee.controller';
+
+
+
---
+++ src/cashier/cashier.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CashierService } from './cashier.service';
+import { CashierController } from './cashier.controller';
+
+@Module({
+  providers: [CashierService],
+  controllers: [CashierController]
+})
+export class CashierModule {}
